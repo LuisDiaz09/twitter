209bfc5b14e6adf5bd6f199178cfd474dea5f86a
Luis <launchx04206@innovaccion.mx>
2022-04-25 20:41:22 -0500

    6. Modificando la prueba para validar que las propiedades 'dateC' y 'lastU' sean de tipo fecha y no recibir un valor undefined

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,12 +2,12 @@ const User = require('./../../app/models/User');
 
 describe ("Creando prueba de unidad para Clase User" , () => {
     test('Creando un objeto de tipo User', () => {
-        const user = new User(1, "luan", "Luis", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "luan", "Luis", "Bio")
         expect(user.id).toBe(1)
         expect(user.username).toBe("luan")
         expect(user.name).toBe("Luis")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     });
 })
