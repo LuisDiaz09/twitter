d4332b7dc548c78ed374605dd75dbe3a4cda497d
Luis <launchx04206@innovaccion.mx>
2022-04-26 18:09:10 -0500

    16. Añadiendo el método getAllUsernames a la clase UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -12,6 +12,11 @@ class UserService{
     static updateUserName(user, username){
         user.setUsername = username
     }
+
+    static getAllUsernames(users){
+        const allusers = users.map(users => users.username)
+        return allusers
+    }
 }
 
 module.exports = UserService
