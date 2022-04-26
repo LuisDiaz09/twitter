90eaab5c6b2d5aba05df83f69d29ad60e0b720e7
Luis <launchx04206@innovaccion.mx>
2022-04-25 21:48:45 -0500

    12. Creando prueba para método getInfo y creando el método en la clase UserService

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,4 +8,13 @@ describe("Creando prueba de unidad para la clase UserService", () =>{
         expect(userservice.name).toBe("Luis")
         expect(userservice.bio).not.toBeUndefined()
     });
+
+    test('Creando un método estático getInfo', () => {
+        const user = UserService.create(1,"luan","Luis")
+        const userInfo = UserService.getInfo(user)
+        expect(userInfo[0]).toBe(1)
+        expect(userInfo[1]).toBe("luan")
+        expect(userInfo[2]).toBe("Luis")
+        expect(userInfo[3]).toBe("Sin Bio")
+    });
 })
