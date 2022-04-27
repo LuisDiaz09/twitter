ba904398056b1e5846b24df541b3df68bc640a77
Luis <launchx04206@innovaccion.mx>
2022-04-26 22:49:31 -0500

    19.Terminando de agregar pruebas de unidad para los requerimientos de la clase UserView y completando el método en la clase

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,10 +1,14 @@
-const User = require ('./../../app/services/UserService')
+const UserService = require ('./../../app/services/UserService')
 
 class UserView {
     static createUser(payload){
         if (payload === null){
             console.log("Error es null")
             return {error: "El payload no existe"}
+        }else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+            return UserService.create(payload.id, payload.username, payload.name)
+          } else{
+            return {error: "necesitan un valor válido"}
         }
     }
 }
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -6,4 +6,24 @@ describe("Prueba de unidad para la clase UserViwe", () => {
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/El payload no existe/)
     });
+
+    test('', () =>{
+        const payload = {username:null, name:12, id: "id"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan un valor válido/)
+    });
+
+    test('Validando que payload regrese un objeto error', () =>{
+        const payload = {username:"Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan un valor válido/)
+    });
+
+    test('Creando usuario al enviar un payload', () => {
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.id).toBe(1)
+        expect(result.username).toBe("username")
+    });
 })
